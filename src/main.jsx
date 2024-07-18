5a9f78d0001cbe02fe54c119e0d16201c07c6d7a
Sylvain Marcos Git <sylvainmarcoszossou@gmail.com>
2024-07-18 13:00:58 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,33 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import Home from './components/Home';
-import AboutPage from './components/AboutPage';
-import Contact from './components/Contact';
-import NotFound from './components/NotFound';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import AboutPage from "./components/AboutPage";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element : <Home />,
+    element: <Home />,
   },
   {
     path: "/about",
-    element : <AboutPage />,
+    element: <AboutPage />,
   },
   {
     path: "/contact",
-    element : <Contact />
+    element: <Contact />,
   },
-  
   {
     path: "*",
-    element : <NotFound />
+    element: <NotFound />,
   },
-])
+]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <RouterProvider router={router}>
-      </RouterProvider>
-  </React.StrictMode>,
-)
-;
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
