6f918b3fb8f5680a66c89991c833b9c32a3ab1c9
Sylvain Marcos Git <sylvainmarcoszossou@gmail.com>
2024-07-20 12:56:26 +0100

    gestion d'erreur de message nodemailer

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -8,26 +8,28 @@ export default async function handler(req, res) {
       return res.status(400).json({ message: 'Tous les champs sont requis' });
     }
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_TO,
-      subject: `Nouveau message de ${name}`,
-      text: message,
-    };
-
     try {
-      await transporter.sendMail(mailOptions);
+      console.log('Envoi de l\'email...'); // Journal de début d'envoi
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+
+      const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_TO,
+        subject: `Nouveau message de ${name}`,
+        text: message,
+      };
+
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email envoyé:', info); // Journal de succès
       res.status(200).json({ message: 'Email envoyé avec succès' });
     } catch (error) {
-      console.error('Erreur lors de l\'envoi de l\'email:', error);
+      console.error('Erreur lors de l\'envoi de l\'email:', error); // Journal d'erreur
       res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'email' });
     }
   } else {
