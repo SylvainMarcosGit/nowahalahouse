bd23e1607b69200055442a74037a015baeb19d73
Sylvain Marcos Git <sylvainmarcoszossou@gmail.com>
2024-07-20 12:28:47 +0100

    envoie de message

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -11,8 +11,8 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.REACT_APP_EMAIL_USER,
+        pass: process.env.REACT_APP_EMAIL_PASS,
       },
     });
 
